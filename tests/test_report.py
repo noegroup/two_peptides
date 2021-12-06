f25c0a4f6dcde72de3fb833d434e81b1392c49df
Andreas Krämer <kraemer.research@gmail.com>
2021-12-06 01:21:35 +0100

    check array shapes

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,12 +1,17 @@
 
-from two_peptides.run import run
+import numpy as np
+from two_peptides.run import run, DEFAULT_DISTANCES
 from torchmdnet.datasets.in_mem_dataset import InMemoryDataset
 
 
 def test_run_and_read(tmpdir):
     run("A", "A", str(tmpdir), test=True)
-    InMemoryDataset(
+    dataset = InMemoryDataset(
         coordglob=str(tmpdir/"test_*_coord.npy"),
         forceglob=str(tmpdir/"test_*_force.npy"),
         embedglob=str(tmpdir/"test_*_embed.npy"),
     )
+    assert dataset[0].pos.shape == (10, 3)
+    assert dataset[0].y.shape == (10, 3)
+    assert dataset[0].z.shape == (10, )
+    assert len(dataset) == len(DEFAULT_DISTANCES) * 10
