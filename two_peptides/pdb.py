f4bf0175ed6a48edf486158a2934feec478a2591
Andreas Krämer <kraemer.research@gmail.com>
2021-12-07 23:03:15 +0100

    small fix

--- two_peptides/pdb.py
+++ two_peptides/pdb.py
@@ -7,18 +7,18 @@ import mdtraj as md
 from .status import submitted
 
 
-def make_pdb(aminoacids1, aminoacids2, outdir):
+def save_pdb(aminoacids1, aminoacids2, outdir):
     system = TwoMiniPeptides(aminoacids1, aminoacids2)
     traj = md.Trajectory(system.positions, topology=system.mdtraj_topology)
-    traj.save_pdb(os.path.join(outdir,f"{aminoacids1}_{aminoacids2}.pdb")
+    traj.save_pdb(os.path.join(outdir, f"{aminoacids1}_{aminoacids2}.pdb")
 
 
 @click.command()
 @click.option("-o", "--outdir", type=click.Path(exists=True, writable=True, dir_okay=True), default="./data"))
 def main(outdir):
     for peptide1, peptide2 in submitted():
-        make_pdb(peptide1, peptide2, outdir)
-        make_pdb(peptide2, peptide1, outdir)
+        save_pdb(peptide1, peptide2, outdir)
+        save_pdb(peptide2, peptide1, outdir)
 
 
 if __name__ == "__main__":
