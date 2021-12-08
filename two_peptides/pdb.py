8d85670146822fd6b7dd49f804f7f1f995e22d87
Andreas Krämer <kraemer.research@gmail.com>
2021-12-08 12:09:34 +0100

    save pdb selection

--- two_peptides/pdb.py
+++ two_peptides/pdb.py
@@ -7,20 +7,22 @@ from two_peptides.status import submitted
 from two_peptides.simulation import TwoPeptideSimulation
 
 
-def save_pdb(aminoacids1, aminoacids2, outdir):
+def save_pdb(aminoacids1, aminoacids2, outdir, selection="beads"):
     sim = TwoPeptideSimulation(aminoacids1, aminoacids2)
     model = sim.model
-    traj = md.Trajectory(model.positions[sim.beads], topology=model.mdtraj_topology.subset(sim.beads))
-    traj.save_pdb(os.path.join(outdir, f"{aminoacids1}_{aminoacids2}.pdb"))
+    selected = {"beads": sim.beads, "protein": model.select("protein")}[selection]
+    traj = md.Trajectory(model.positions[selected], topology=model.mdtraj_topology.subset(selected))
+    traj.save_pdb(os.path.join(outdir, f"{aminoacids1}_{aminoacids2}_{selection}.pdb"))
 
 
 @click.command()
 @click.option("-o", "--outdir", type=click.Path(exists=True, writable=True, dir_okay=True), default="./data")
-def main(outdir):
+@click.option("-s", "--selection", type=click.Choice(["beads", "protein"]), default="beads")
+def main(outdir, selection):
     for peptide1, peptide2 in submitted():
         print(peptide1, peptide2)
-        save_pdb(peptide1, peptide2, outdir)
-        save_pdb(peptide2, peptide1, outdir)
+        save_pdb(peptide1, peptide2, outdir, selection)
+        save_pdb(peptide2, peptide1, outdir, selection)
 
 
 if __name__ == "__main__":
