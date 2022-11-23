97e411aa5f862015dd90c6a15d9ba49420237261
Andreas Krämer <kraemer.research@gmail.com>
2022-11-23 11:18:15 +0100

    better sanity check

--- two_peptides/sanity.py
+++ two_peptides/sanity.py
@@ -1,5 +1,5 @@
 """Sanity checks"""
-
+import glob
 import os
 import dataclasses
 from typing import Union
@@ -91,10 +91,12 @@ class EquilibrationStats:
 
 
 @click.command(name="sanity-check")
-@click.option("-a", "--aminoacids1", type=click.Choice(VALID_PEPTIDES))
-@click.option("-b", "--aminoacids2", type=click.Choice(VALID_PEPTIDES))
+@click.option("--many/--no-many", default=False)
+@click.option("-a", "--aminoacids1", type=click.Choice(VALID_PEPTIDES), default=None)
+@click.option("-b", "--aminoacids2", type=click.Choice(VALID_PEPTIDES), default=None)
 @click.option("-o", "--outdir", type=click.Path(exists=True, dir_okay=True, writable=True), default="./data")
 def sanity_check_cmd(
+        many: bool,
         aminoacids1: str,
         aminoacids2: str,
         outdir: str = "./data",
@@ -103,14 +105,44 @@ def sanity_check_cmd(
     Example: `two_peptides run -a AA -b YY` will run umbrella sampling between dialanine and dityrosine
     and save the results to a subdirectory data.
     """
-    equi_file = EquilibrationStats.from_array(np.load(os.path.join(outdir, f"{aminoacids1}_{aminoacids2}_equilibration.txt")))
-    density_ok = check_up_down(equi_file.density[300:600])
-    density_unity = equi_file.density[-1] > 0.95 and equi_file.density[-1] > 1.05
-    print(density_ok, density_unity)
+
+    print("      rho    se   drift <3se   d0     T    se")
+
+    peptide_pairs = ((aminoacids1, aminoacids2), )
+    if many:
+        files = glob.glob(os.path.join(outdir, f"*_*_equilibration.txt"))
+        peptide_pairs = (filename.split("_")[:2] for filename in files)
+
+    for peptide1, peptide2 in peptide_pairs:
+        print(f"{peptide1:>2} {peptide2:>2} ", end="")
+
+        checks = []
+
+        equilibration_file = os.path.join(outdir, f"{aminoacids1}_{aminoacids2}_equilibration.txt")
+        equilibration_stats = EquilibrationStats.from_array(np.loadtxt(equilibration_file))
+
+        # density
+        production = equilibration_stats.production.astype(bool)
+        mean, se, drift = mean_se_drift(equilibration_stats.density[150:300])
+        checks.append(np.allclose(mean, 1.0, atol=0.02))
+        checks.append(np.abs(drift) < 0.005)
+        checks.append(se < 0.005)
+        checks.append(np.abs(drift) < 3 * se)
+
+        # distance
+        checks.append(
+            np.percentile(np.abs(equilibration_stats.d[production] - equilibration_stats.d0[production]), 99) < 0.2)
+
+        # temperature
+        mean, se, drift = mean_se_drift(equilibration_stats.temperature[production])
+        checks.append(np.allclose(mean, 300., atol=1.0))
+        checks.append(se < 1.0)
+
+        print(" ".join("  .  " if check else " XXX " for check in checks))
 
 
-def check_up_down(time_series, tol=0.1):
-    increase = time_series[1:] > time_series[:-1] + 1e-10
-    decrease = time_series[1:] < time_series[:-1] - 1e-10
-    increase_fraction = increase.sum() / decrease.sum()
-    return increase_fraction > 1. - tol and increase < 1. + tol
+def mean_se_drift(ts, n=5):
+    chunks = np.array_split(ts, n)
+    means = [np.mean(chunk) for chunk in chunks]
+    a, b, *_ = np.polyfit(np.arange(5), means, 1)
+    return np.mean(means), np.std(means)/np.sqrt(5), np.mean(np.abs([a*0+b - means[0], a*4+b - means[4]]))
